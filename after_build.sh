646e37e4e274f6147e7779516ac3f0d622227edf
littletao08 <cvghju@163.com>
2019-11-22 17:06:58 +0800

    Update after_build.sh
    删除无用固件

--- after_build.sh
+++ after_build.sh
@@ -2,6 +2,7 @@
 cd /home/runner/openwrt
 rm -rf bin/packages
 cd bin/targets/x86/64
+rm -rf packages
 rm -rf  openwrt-x86-64-rootfs-squashfs.img openwrt-x86-64-combined-squashfs.vhdx openwrt-x86-64-rootfs-squashfs.img
 
 # Enter your commands here, e.g.
